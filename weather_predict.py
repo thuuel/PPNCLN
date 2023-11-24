b4082259a14d8af43c201cbb685f5b7e330ff018
thuuel <151109735+thuuel@users.noreply.github.com>
2023-11-24 12:31:38 +0700

    Update weather_predict.py

--- weather_predict.py
+++ weather_predict.py
@@ -243,16 +243,25 @@ st.write(f'Chỉ số Accuracy của mô hình là {a}')
 
 with st.sidebar:
     st.subheader("Vui lòng nhập các thông số dưới đây!")
-    temp_text = st.text_input("Nhiệt độ trung bình (độ F)")
-    
-    dewpoint_text = st.text_input("Nhiệt độ điểm sương trung bình (độ F)")
-    
+    temp_text = st.text_input("Nhiệt độ trung bình (độ F)") 
+    dewpoint_text = st.text_input("Nhiệt độ điểm sương trung bình (độ F)") 
     humidty_text = st.text_input("Độ ẩm trung bình (%)")
-
     slp_text = st.text_input("Áp suất trung bình ở mực nước biển (inch)")
-
     visibility_text = st.text_input("Tầm nhìn trung bình (m)")
-
     windspeed_text = st.text_input("Tốc độ gió trung bình (m/h)")
-
     precipi_text = st.text_input("Tổng lượng mưa (inch)")
+    data_input = {
+    'TempAvgF': [temp_text],
+    'DewPointAvgF': [dewpoint_text],
+    'HumidityAvgPercent': [humidty_text],
+    'SeaLevelPressureAvgInches': [slp_text],
+    'VisibilityAvgMiles': [visibility_text],
+    'WindAvgMPH': [windspeed_text],
+    'PrecipitationSumInches': [precipi_text]}
+    input_df = pd.DataFrame(data_input)
+    if st.button('Predict'):
+        distancedf_input = get_distances_from_cluster(input_df)
+        result_events = classify_events(distancedf_input)
+        true_columns = result_events.apply(lambda row: row.index[row].tolist(), axis=1)
+        st.write(f"Với nhhững thông số trên, hôm ấy trời có {true_columns}")
+
